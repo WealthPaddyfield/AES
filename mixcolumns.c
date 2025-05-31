574e98567085c2609a34ebc4d6580a92b442f2af
T.Masato <5912masato@gmail.com>
2025-05-31 16:34:07 +0900

    05/31

--- mixcolumns.c
+++ mixcolumns.c
@@ -1,2 +1,25 @@
 #include <stdio.h>
 #include <stdint.h>
+
+unsigned char state [4] [4] = {
+    0x00, 0x01, 0x02, 0x03,
+    0x04, 0x05, 0x06, 0x07,
+    0x08, 0x09, 0x0A, 0x0B,
+    0x0C, 0x0D, 0x0E, 0x0F
+};
+
+void print_state(unsigned char state[4][4]) {
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            printf("%02X ", state[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+int mixcolumns
+
+
+int main(){
+    return 0;
+}
