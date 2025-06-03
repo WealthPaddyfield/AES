e9c7177f8df6ead72b89a4c002d426035ca0fa67
T.Masato <5912masato@gmail.com>
2025-06-03 16:57:50 +0900

    base parts ready

--- mixcolumns.c
+++ mixcolumns.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdint.h>
 
-unsigned char state [4] [4] = {
+/*unsigned char in_state [4] [4] = {
     0x00, 0x01, 0x02, 0x03,
     0x04, 0x05, 0x06, 0x07,
     0x08, 0x09, 0x0A, 0x0B,
     0x0C, 0x0D, 0x0E, 0x0F
-};
+};*/
 
 void print_state(unsigned char state[4][4]) {
     for (int i = 0; i < 4; i++) {
@@ -17,9 +17,23 @@ void print_state(unsigned char state[4][4]) {
     }
 }
 
-int mixcolumns
+int mixcolumns(unsigned char state[4][4]) {
+    unsigned char temp[4][4];
+    const unsigned char matrix[4][4] = {
+       0x02,0x03,0x01,0x01,
+       0x01,0x02,0x03,0x01,
+       0x01,0x01,0x02,0x03,
+       0x03,0x01,0x01,0x02
+    };
+
+    
+
+    return 0;    
+
+}
 
 
 int main(){
-    return 0;
+    mixcolumns(state);
+    print_state(state);
 }
