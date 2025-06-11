bdd4089e70d409f7cd1f082e579af81bede87fd7
T.Masato <5912masato@gmail.com>
2025-06-11 11:55:22 +0900

    added addroundkey.c

---
+++ addroundkey.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+
+//for test
+unsigned char in_state [4] [4] = {
+    0x00, 0x01, 0x02, 0x03,
+    0x04, 0x05, 0x06, 0x07,
+    0x08, 0x09, 0x0A, 0x0B,
+    0x0C, 0x0D, 0x0E, 0x0F
+};
+
+void print_state(unsigned char state[4][4]) {
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            printf("%02X ", state[i][j]);
+        }
+        printf("\n");
+    }
+}
+
+void addroundkey(unsigned char state[4][4]){
+
+}
