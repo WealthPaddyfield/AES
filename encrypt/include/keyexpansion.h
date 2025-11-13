49c6065f255dea48cf627d1f709b9b331f28405a
T.Masato <5912masato@gmail.com>
2025-11-13 18:51:25 +0900

    +func changeable key length

--- encrypt/include/keyexpansion.h
+++ encrypt/include/keyexpansion.h
@@ -1,3 +1,4 @@
 #pragma once
+#include "aes_key_config.h"
 
-void keyexpansion(unsigned char key[4][4], unsigned char w[44][4]);
+void keyexpansion(unsigned char key[4][AES_NK], unsigned char w[AES_WORDS][4]);
